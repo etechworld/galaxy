74bf38d9b9b7cb83ceb530a4413206d4f2c0bc75
amitanurup <amitanurup@gmail.com>
2026-07-03 03:07:11 +0530

    Update version string to v2.20 in UI, v2.20.1

--- src/App.tsx
+++ src/App.tsx
@@ -290,7 +290,7 @@ function LoginScreen({
           <div>
             <h1 id="login-title">Galaxy Cartridge Care</h1>
             <p>Service receive, repair update, inventory</p>
-            <span className="dev-credit">Developed by PC WORLD | v2.15</span>
+            <span className="dev-credit">Developed by PC WORLD | v2.20</span>
           </div>
         </div>
 
@@ -547,7 +547,7 @@ function AppShell({
         <div className="dev-credit-sidebar">
           Galaxy Cartridge Care
           <br />
-          <small>Developed by PC WORLD | v2.15</small>
+          <small>Developed by PC WORLD | v2.20</small>
         </div>
 
         <div className="user-card">
