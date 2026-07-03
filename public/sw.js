74205ae9ebec086a48a0d73f376fe9a3ff399275
amitanurup <amitanurup@gmail.com>
2026-07-03 20:13:05 +0530

    Fix sw.js cross-origin intercept to allow proper error fallbacks, v2.32

--- public/sw.js
+++ public/sw.js
@@ -7,6 +7,10 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  // Simple pass-through fetch
+  const url = new URL(e.request.url);
+  // Do not intercept cross-origin API requests
+  if (url.origin !== self.location.origin) {
+    return;
+  }
   e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
 });
